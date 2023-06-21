a53e5fdf932436e96a61103193693b596fef41e9
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-22 03:09:49 +0530

    added component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import LandingPage from "@/components/LandingPage";
+import Project1 from "@/components/Projects/Project1";
 import Tagline from "@/components/Tagline";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="min-h-screen overflow-x-hidden">
       <LandingPage />
       <Tagline />
+      <Project1 />
     </main>
   )
 }
