5e124c94d34b5d56b3f7fbb94de8dbf816f642b1
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-22 02:10:22 +0530

    added component

---
+++ components/Tagline.tsx
@@ -0,0 +1,40 @@
+import { Lexend_Deca } from 'next/font/google'
+
+const lexend = Lexend_Deca({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+const Tagline = () => {
+    return (
+        <div
+        className="relative w-screen h-screen flex flex-col justify-center items-center bg-[#031226] text-white"
+        >
+            <div
+            className='flex flex-col justify-center items-center w-4/6 gap-[28px]'
+            >
+                <p
+                className="anuratiFont text-xl"
+                >SYNAPSE</p>
+
+                <h2
+                className={`font-bold text-[69px] leading-[112px] tracking-[8.4px] uppercase ${lexend.className}`}
+                >Innovative Cohort</h2>
+
+                <p
+                className='text-xl text-center'
+                >Where talented college students unite, leveraging cutting-edge tech stacks to deliver innovative solutions, showcasing their boundless potential.</p>
+            </div>
+
+            <button
+            className='absolute bottom-0 mb-[20px] w-[70px] h-[82px] flex items-center justify-center bg-center bg-contain bg-[url(/images/downBtn.png)] bg-no-repeat'
+            >
+                <div
+                className='w-[23px] h-[12px] bg-center bg-contain bg-[url(/images/down.png)] bg-no-repeat'
+                ></div>
+            </button>
+        </div>
+    )
+}
+
+export default Tagline
