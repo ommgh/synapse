11338380e371fd7aa8ba2c1dbba044c16c3b8c31
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-24 23:22:18 +0530

    added footer

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,12 +1,12 @@
 import { poppins } from "@/lib/fonts"
 import Hero from "./Hero"
 import About from "./About"
-import ContactUs from "./ContactUs"
 import Project from "./Projects/Project"
 import Skills from "./Skills"
 import Tagline from "./Tagline"
 import Team from "./Team"
 import Services from "./Services"
+import Footer from "./Footer"
 
 const LandingPage = () => {
     return (
@@ -18,7 +18,7 @@ const LandingPage = () => {
             {/* <Skills />
             <Team /> */}
             <Services />
-            <ContactUs />
+            <Footer />
         </>
     )
 }
