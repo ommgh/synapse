f4241fa839b7b7eff7b44eaf656fb4f0bfb1f05f
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-25 20:24:16 +0530

    added animation and updated styling

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,5 +1,7 @@
 import { faqs } from "@/lib/data";
+import Image from "next/image";
 import { useState } from "react";
+import { AttentionSeeker } from 'react-awesome-reveal'
 
 interface Queries {
     id: number,
@@ -14,6 +16,37 @@ const FAQ = () => {
         <div
         className="w-full p-[70px]"
         >
+            <div
+            className="w-fit flex m-auto mb-[0px]"
+            >
+                <AttentionSeeker
+                effect="wobble"
+                triggerOnce={false}
+                className="z-[2]"
+                >
+                    <Image
+                    src={'https://res.cloudinary.com/db7nrltsv/image/upload/v1690294091/Untitled_design_11_cigk4r.png'}
+                    width={50}
+                    height={50}
+                    alt="question mark"
+                    className="rotate-[-50deg]"
+                    />
+                </AttentionSeeker>
+
+                <AttentionSeeker
+                effect="wobble"
+                delay={500}
+                triggerOnce={false}
+                >
+                    <Image
+                    src={'https://res.cloudinary.com/db7nrltsv/image/upload/v1690294091/Untitled_design_11_cigk4r.png'}
+                    width={50}
+                    height={50}
+                    alt="question mark"
+                    className="rotate-[-10deg] mt-[30px]"
+                    />
+                </AttentionSeeker>
+            </div>
             <p
             className="text-center text-5xl leading-[110px] font-semibold tracking-wide mb-[50px]"
             >Have some queries, we got you!</p>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div
         className="relative h-screen w-screen flex flex-col justify-center items-center pt-[60px] select-none"
         >
-            {/* <Fade direction="left"
+            <Fade direction="left"
             className="absolute left-0 w-[350px] h-[350px]"
             >
                 <Image 
@@ -27,25 +27,37 @@ const Hero = () => {
             </Fade>
 
             <Fade direction="right"
-            className="absolute w-[350px] h-[350px] border-2 border-black"
+            cascade
+            delay={500}
+            className="absolute top-[150px] left-[75%] w-[350px] h-[350px]"
             >
                 <Image 
                 src={"https://res.cloudinary.com/db7nrltsv/image/upload/v1690233747/Untitled_design_7_k7g3vx.png"}
-                width={350}
-                height={350}
+                width={250}
+                height={250}
                 alt="doodle triangle"
-                className="absolute right-[-150px] border-2 border-black"
+                className="absolute right-[-150px] rotate-[30deg]"
                 />
-            </Fade> */}
 
-            <div
+                <Image 
+                src={"https://res.cloudinary.com/db7nrltsv/image/upload/v1690295351/Untitled_design_12_jknmkm.png"}
+                width={100}
+                height={100}
+                alt="doodle triangle"
+                className="absolute top-[300px] right-[-50px] rotate-[30deg]"
+                />
+            </Fade>
+
+            <Fade
+            cascade
+            direction="up"
+            triggerOnce
+            delay={1500}
             className="flex flex-col justify-center items-center gap-[30px]"
             >
-                <Fade
+                <div
                 className="anuratiFont w-[1008px] text-center text-[124px] mt-[-30px] mb-[-30px] font-normal uppercase tracking-widest"
-                direction="up"
-                triggerOnce
-                >SYNAPSE</Fade>
+                >SYNAPSE</div>
 
                 <p
                 className={`w-[828.27px] text-center text-[22px] font-semibold mb-[30px] ${poppins.className}`}
@@ -56,33 +68,7 @@ const Hero = () => {
                 >
                     Start Building
                 </button>
-            </div>
-
-            {/* <div
-            className=" w-screen flex justify-center items-center my-[50px] z-[-1]"
-            >
-                {illustrations.map((values: Illustrations, id: number) => (
-                    <>
-                        {id==4 && 
-                                <div
-                                className="w-[240px] h-[150px] "
-                                >&nbsp;</div>
-                            }
-                        <div
-                        className={` w-[300px] h-[275px] flex justify-center items-center border-2 border-black z-[-1]`}
-                        key={id}
-                        >
-                            <Image 
-                            src={values.url}
-                            alt="illustrations"
-                            width={300}
-                            height={300}
-                            className={id >= 4 ? "transform -scale-x-100 ": "border-2 border-black"}
-                            />
-                        </div>
-                    </>
-                ))}
-            </div> */}
+            </Fade>
         </div>
     )
 }
