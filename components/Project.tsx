d34d4101afd68d0d1f44a81d40f897786fb91250
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-25 17:41:44 +0530

    added project box

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,6 +2,8 @@ import { projectData } from "@/lib/data";
 import { poppins } from "@/lib/fonts";
 import Image from "next/image";
 import Link from "next/link";
+import ProjectBox from "./ProjectBox";
+import { useState } from "react";
 
 interface Project {
     id: number,
@@ -13,9 +15,11 @@ interface Project {
 }
 
 const Project = () => {
+    const [showBox, setShowBox] = useState<boolean>(false)
+    const [boxData, setBoxData] = useState<Project>(projectData[0])
     return (
         <div
-        className={`w-fit px-[90px] my-[80px] flex flex-col justify-center items-center ${poppins.className}`}
+        className={`relative w-fit px-[90px] my-[80px] flex flex-col justify-center items-center ${poppins.className}`}
         >
             <div
             className="mb-[-80px] z-[1] flex justify-center items-center gap-[300px]"
@@ -45,23 +49,31 @@ const Project = () => {
             className="grid grid-flow-row lg:grid-cols-2 md:grid-cols-1 justify-center items-center mt-[70px] gap-[200px]"
             >
                 {projectData.map((project: Project, id: number) => (
-                    <div className="flex flex-col items-center" key={id}>
-                        <div
-                        className="w-[350px] h-[400px] flex justify-center items-center overflow-hidden hover:cursor-pointer"
-                        >
-                            <Image
-                            src={project.img}
-                            width={350}
-                            height={400}
-                            alt="project banner"
-                            />
-                        </div>
+                    <>
+                        <div className="flex flex-col items-center" key={id}>
+                            <div
+                            className="w-[350px] h-[400px] flex justify-center items-center overflow-hidden hover:cursor-pointer"
+                            onClick={() => {
+                                setBoxData(project)
+                                setShowBox(true)
+                            }}
+                            >
+                                <Image
+                                src={project.img}
+                                width={350}
+                                height={400}
+                                alt="project banner"
+                                />
+                            </div>
 
-                        <Link
-                        className="text-2xl font-semibold tracking-wider my-[20px]"
-                        href={project.website}
-                        >{project.name}</Link>
-                    </div>
+                            <Link
+                            className="text-2xl font-semibold tracking-wider my-[20px]"
+                            href={project.website}
+                            >{project.name}</Link>
+
+                            {showBox && <ProjectBox project={boxData} setShowBox={setShowBox} />}
+                        </div>
+                    </>
                 ))}
             </div>
         </div>
---
+++ components/ProjectBox.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ProjectBox = ({project, setShowBox}: any) => {
+    return (
+        <div
+        className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-white z-[4] p-[70px]"
+        >
+            <div
+            className="relative w-1/2 h-full flex flex-col justify-center items-center gap-[50px] pr-9"
+            >
+                <button
+                className="absolute top-0 left-0 px-4 py-2 text-lg border-2 border-transparent rounded-lg hover:border-black"
+                onClick={() => setShowBox(false)}
+                >Close</button>
+
+                <p
+                className="w-full text-left text-4xl font-semibold tracking-wide"
+                >{project.name}</p>
+
+                <p
+                className="w-full text-left text-xl font-medium"
+                >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, iure omnis id, ducimus maxime quia deleniti provident ab eaque dolorum dolorem rem debitis ut? Eveniet debitis quae sed ipsa quo.</p>
+
+                <ul
+                className="text-xl font-medium flex gap-[30px] tracking-wide"
+                >
+                    <Link href={project.website}
+                    className="hover:font-semibold"
+                    >Website</Link>
+
+                    <Link href={project.github}
+                    className="hover:font-semibold"
+                    >Github Repo</Link>
+                </ul>
+            </div>
+
+            <div
+            className="w-1/2 h-full overflow-hidden"
+            >
+                <Image
+                src={project.img}
+                width={700}
+                height={900}
+                alt="project banner"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ProjectBox;
