4e7abaeae3186fa06ea416c3d4c433853baaf865
Anushree <anushreejaiswal.srn@gmail.com>
2023-08-03 09:31:02 +0530

    updated responsive styling

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,7 +1,6 @@
 import { projectData } from '@/lib/data'
 import { poppins } from '@/lib/fonts'
 import Image from 'next/image'
-import Link from 'next/link'
 import ProjectBox from './ProjectBox'
 import { useState } from 'react'
 import { Bounce, Fade } from 'react-awesome-reveal'
@@ -20,21 +19,22 @@ const Project = () => {
     const [showBox, setShowBox] = useState<boolean>(false)
     const [project, setProject] = useState<Project>(projectData[0])
     const [index, setIndex] = useState<number>(-1)
+
     return (
         <div
-            className={`flex flex-col min-h-screen pt-[50px] pb-[10px] px-[70px] w-screen justify-center items-center ${poppins.className}`}
+            className={`flex flex-col min-h-screen pt-[50px] pb-[10px] px-[70px] w-screen justify-center items-center max-[600px]:px-[20px] max-[600px]:mb-[50px] ${poppins.className}`}
             id="projects"
         >
             <Bounce className="my-[40px]" triggerOnce>
-                <p className="text-center text-6xl leading-[90px] font-semibold tracking-wider">
+                <p className="text-center text-6xl leading-[90px] font-semibold tracking-wider max-[700px]:w-full max-[700px]:text-5xl max-[700px]:leading-[75px] max-[500px]:text-4xl max-[500px]:leading-[60px]">
                     Want to see what <br /> innovative minds can create
                 </p>
             </Bounce>
 
             <div
-                className={`relative w-fit px-[90px] my-[80px] flex flex-col justify-center items-center ${poppins.className}`}
+                className={`relative w-fit px-[90px] my-[80px] flex flex-col justify-center items-center max-[600px]:px-[20px] max-[700px]:mt-[40px] ${poppins.className}`}
             >
-                <div className="mb-[-80px] z-[1] flex justify-center items-center gap-[300px]">
+                <div className="mb-[-80px] z-[1] flex justify-center items-center gap-[300px] max-[830px]:gap-[160px] max-[700px]:mb-[-70px] max-[500px]:mb-[-60px]">
                     <Fade direction="left">
                         <Image
                             src={
@@ -61,12 +61,12 @@ const Project = () => {
                 </div>
 
                 <Fade delay={500} triggerOnce>
-                    <p className="text-5xl text-center font-semibold tracking-wider mb-[50px]">
+                    <p className="text-5xl text-center font-semibold tracking-wider mb-[50px] max-[830px]:text-3xl">
                         Our Work
                     </p>
                 </Fade>
 
-                <div className="grid grid-flow-row lg:grid-cols-2 md:grid-cols-1 justify-center items-center mt-[70px] gap-[100px]">
+                <div className="grid grid-flow-row grid-cols-2 justify-center items-center mt-[70px] gap-[100px] max-[1330px]:grid-cols-1 max-[600px]:gap-[60px]">
                     {projectData.map((data: Project, id: number) => (
                         <>
                             {showBox && index == id && (
@@ -85,7 +85,7 @@ const Project = () => {
                             <Fade direction="up" triggerOnce cascade key={id}>
                                 <div className="flex flex-col items-center">
                                     <div
-                                        className="w-[533px] h-[300px] flex justify-center items-center overflow-hidden hover:cursor-pointer"
+                                        className="w-[533px] h-[300px] flex justify-center items-center overflow-hidden hover:cursor-pointer max-[600px]:w-full max-[600px]:h-fit bg-red-300"
                                         onClick={() => {
                                             setProject(data)
                                             setIndex(id)
@@ -100,7 +100,7 @@ const Project = () => {
                                         />
                                     </div>
 
-                                    <p className="text-2xl font-semibold tracking-wider my-[20px]">
+                                    <p className="text-2xl text-center font-semibold tracking-wider my-[20px] max-[600px]:my-[10px] max-[600px]:text-xl">
                                         {data.name}
                                     </p>
                                 </div>
